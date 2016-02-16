7ca2406aace88e21e9df9ff06d39408601318aed
bonecrushereb <bonecrushereb@gmail.com>
2016-02-16 11:54:37 -0800

    updated randomness for pizza and delivaries. Updating for assignment submission

--- app.js
+++ app.js
@@ -1,114 +1,5 @@
 'use strict'
 
-//8am-11am pizza/hr
-function p_h8_11() {
-  var p8_11min = 0;
-  var p8_11max = 4;
-  var result = Math.floor(Math.random() * (p8_11max - p8_11min + 1)) + p8_11min;
-  console.log('8am-11am pizza/hour has a number of ' + result);
-  return result;
-}
-
-//8-11am delivery/hr
-function d_h8_11() {
-  var d8_11min = 0;
-  var d8_11max = 4;
-  var result = Math.floor(Math.random() * (d8_11max - d8_11min + 1)) + d8_11min;
-  console.log('8am-11am delivery/hour has a number of ' + result);
-  return result;
-}
-
-//11am-2pm pizza/hr
-function p_h11_2(){
-  var p11_2min = 0;
-  var p11_2max = 7;
-  var result = Math.floor(Math.random() * (p11_2max - p11_2min + 1)) +p11_2min;
-  console.log('11am-2pm pizza/hour has a number of ' + result);
-  return result;
-}
-
-//11am-2pm delivery/hr
-function d_h11_2() {
-  var d11_2min = 0;
-  var d11_2max = 4;
-  var result = Math.floor(Math.random() * (d11_2max - d11_2min + 1)) + d11_2min;
-  console.log('11am-2pm delivery/hour has a number of ' + result);
-  return result;
-}
-
-//2pm-5pm pizza/hr
-function p_h2_5(){
-  var p2_5min = 2;
-  var p2_5max = 15;
-  var result = Math.floor(Math.random() * (p2_5max - p2_5min + 1)) +p2_5min;
-  console.log('2pm-5pm pizza/hour has a number of ' + result);
-  return result;
-}
-
-//2pm-5pm delivery/hr
-function d_h2_5() {
-  var d2_5min = 1;
-  var d2_5max = 4;
-  var result = Math.floor(Math.random() * (d2_5max - d2_5min + 1)) + d2_5min;
-  console.log('2pm-5pm delivery/hour has a number of ' + result);
-  return result;
-}
-
-//5pm-8pm pizza/hr
-function p_h5_8(){
-  var p5_8min = 15;
-  var p5_8max = 35;
-  var result = Math.floor(Math.random() * (p5_8max - p5_8min + 1)) +p5_8min;
-  console.log('5pm-8pm pizza/hour has a number of ' + result);
-  return result;
-}
-
-//5pm-8pm delivery/hr
-function d_h5_8() {
-  var d5_8min = 3;
-  var d5_8max = 8;
-  var result = Math.floor(Math.random() * (d5_8max - d5_8min + 1)) + d5_8min;
-  console.log('5pm-8pm delivery/hour has a number of ' + result);
-  return result;
-}
-
-//8pm-11pm pizza/hr
-function p_h8_11(){
-  var p8_11min = 15;
-  var p8_11max = 35;
-  var result = Math.floor(Math.random() * (p8_11max - p8_11min + 1)) +p8_11min;
-  console.log('8pm-11pm pizza/hour has a number of ' + result);
-  return result;
-}
-
-//8pm-11pm delivery/hr
-function d_h8_11() {
-  var d8_11min = 3;
-  var d8_11max = 8;
-  var result = Math.floor(Math.random() * (d8_11max - d8_11min + 1)) + d8_11min;
-  console.log('8pm-11pm delivery/hour has a number of ' + result);
-  return result;
-}
-
-//11pm-2am pizza/hr
-function p_h11p_2p(){
-  var p11_2min = 5;
-  var p11_2max = 20;
-  var result = Math.floor(Math.random() * (p11_2max - p11_2min + 1)) +p11_2min;
-  console.log('11pm-2am pizza/hour has a number of ' + result);
-  return result;
-}
-
-//11pm-2am delivery/hr
-function d_h11p_2p() {
-  var d11_2min = 6;
-  var d11_2max = 11;
-  var result = Math.floor(Math.random() * (d11_2max - d11_2min + 1)) + d11_2min;
-  console.log('11pm-2am delivery/hour has a number of ' + result);
-  return result;
-}
-
-
 var shop1 = document.getElementById('ballard');
 var shop2 = document.getElementById('fistHill');
 var shop3 = document.getElementById('internDist');
@@ -116,18 +7,20 @@ var shop4 = document.getElementById('sLU');
 var shop5 = document.getElementById('georgetown');
 var shop6 = document.getElementById('ravenna');
 
-var timeList =['8am-11am','11am-2pm','2pm-5pm','5pm-8pm','8pm-11pm','11pm-2am'];
+var timeList =['8:00am','9:00am','10:00am','11:00am','12:00 Noon','1:00pm','2:00pm','3:00pm','4:00pm','5:00pm','6:00pm','7:00pm','8:00pm','9:00pm','10:00pm','11:00pm','12:00 Midnight','2:00am'];
+
 var ballard = {
   name: 'pizza3001',
   location: 'Ballard',
+  pizza:[],
+  delivery:[],
   schedule: function(){
-  //for loop to iterate thru the arrays
+    //for loop to iterate thru the arrays
     for(var i = 0; i < timeList.length; i++){
-  //build li element
+      //build li element
       var liEl = document.createElement('li');
       liEl.textContent = timeList[i];
-
-  //put it in the DOM
+      //put it in the DOM
       shop1.appendChild(liEl);
     }
   }
@@ -137,6 +30,8 @@ ballard.schedule();
 var firstHill = {
   name: 'pizza3001',
   location: 'First Hill',
+  pizza:
+  delivery:
   schedule: function(){
   //for loop to iterate thru the arrays
     for(var i = 0; i < timeList.length; i++){
@@ -218,16 +113,23 @@ var ravenna = {
   }
 };
 ravenna.schedule();
-// function listify(listedTimes){
-// //for loop to iterate thru the arrays
-//   for(var i = 0; i < listedTimes.length; i++){
-// //build li element
-//     var liEl = document.createElement('li');
-//     liEl.textContent = listedTimes[i];
-//
-// //put it in the DOM
-//     placeForMyList.appendChild(liEl);
-//   }
-// }
-//
-// listify(shop1.ballard);
+
+function randomGenerator(min,max) {
+  var result = Math.floor(Math.random() * (max - min + 1)) + min;
+  console.log('The random number is ' + result);
+  return result;
+}
+
+
+var pizza = [[0,4],[0,7],[2,15],[15,35],[12,31],[5,20]];
+var delivery = [[0,4],[0,4],[1,4],[3,8],[5,12],[6,11];
+var pizzzProbabillit = [];
+var deliveryProb = [];
+
+for (var i = 0; i < pizza.length; i++) {
+  randomGenerator(pizza[i][0],pizza[i][1]);
+}
+
+for(var i = 0; i < delivery.length; i++){
+  randomGenerator(delivery[i][0],delivery[i][1]);
+}
